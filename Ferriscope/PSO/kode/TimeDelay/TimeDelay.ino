faeb4ce3a744f5f3ea2e81ab677757db5f7fd211
jenschr <jenschr@gmail.com>
2022-09-20 21:22:55 +0200

    Timer tweaks + support for pot to adjust speed

--- Ferriscope/PSO/kode/TimeDelay/TimeDelay.ino
+++ Ferriscope/PSO/kode/TimeDelay/TimeDelay.ino
@@ -1,3 +1,6 @@
+// Based on an Adafruit RP2040 board, but any
+// Feather board can do this...
+
 int delayBeforeStarting5V = 10000;
 int transistorPin = 12;
 
@@ -5,14 +8,27 @@ void setup() {
   Serial.begin(115200);
   pinMode( transistorPin, OUTPUT );
   pinMode( LED_BUILTIN, OUTPUT );
+
   digitalWrite( transistorPin, HIGH );
   digitalWrite( LED_BUILTIN, HIGH );
   Serial.println("on");
-  delay(2000);
+  delay(5000);
   digitalWrite( transistorPin, LOW );
   digitalWrite( LED_BUILTIN, LOW );
   Serial.println("off");
-  delay( delayBeforeStarting5V );
+  delay( 5000 );
+  digitalWrite( transistorPin, HIGH );
+  digitalWrite( LED_BUILTIN, HIGH );
+  Serial.println("on");
+
+  digitalWrite( transistorPin, HIGH );
+  digitalWrite( LED_BUILTIN, HIGH );
+  Serial.println("on");
+  delay(5000);
+  digitalWrite( transistorPin, LOW );
+  digitalWrite( LED_BUILTIN, LOW );
+  Serial.println("off");
+  delay( 2000 );
   digitalWrite( transistorPin, HIGH );
   digitalWrite( LED_BUILTIN, HIGH );
   Serial.println("on");
--- Venetie/VenetieDiscokule/VenetieDiscokule.ino
+++ Venetie/VenetieDiscokule/VenetieDiscokule.ino
@@ -1,5 +1,6 @@
 #include <AccelStepper.h>
 
+#define MOTOR_SPEED_PIN A0
 #define MOTOR_STEP_PIN 2
 #define MOTOR_SIGNAL_PIN 3
 #define MOTOR_ANTICLOCKWISE_PIN 4
@@ -10,26 +11,54 @@ AccelStepper stepper(AccelStepper::FULL2WIRE, MOTOR_STEP_PIN, MOTOR_ANTICLOCKWIS
 
 int previousSignal = 0;
 int currentSignal = 0;
+int prevPot = 0;
+int scaledSpeed = 0;
 
 void setup() {
+  //Serial.begin(9600);
   pinMode( MOTOR_SIGNAL_PIN, INPUT );
   pinMode( MOTOR_STEP_PIN, OUTPUT );
   pinMode( MOTOR_ANTICLOCKWISE_PIN, OUTPUT );
   pinMode( MOTOR_DISABLE_PIN, OUTPUT );
+  pinMode( MOTOR_SPEED_PIN, INPUT );
 
   digitalWrite( MOTOR_STEP_PIN, LOW );
   digitalWrite( MOTOR_ANTICLOCKWISE_PIN, LOW );
   digitalWrite( MOTOR_DISABLE_PIN, LOW );
 
-  stepper.setMaxSpeed(400.0);
+  stepper.setMaxSpeed(60 + 5); // adjust rotation speed here
   stepper.setAcceleration(100.0);
 }
 
 void loop() {
+  int pot = analogRead(MOTOR_SPEED_PIN);
+  if ( pot != prevPot )
+  {
+    scaledSpeed = map( pot, 0, 1023, 0, 10000 );
+    stepper.setMaxSpeed(scaledSpeed + 1);
+    /*
+    Serial.print("*");
+    Serial.print(pot);
+    Serial.print(":");
+    Serial.print(scaledSpeed + 5);
+    Serial.println(" ");
+    */
+  }
+  else
+  {
+    /*
+      Serial.print(pot);
+      Serial.print(":");
+      Serial.print(prevPot);
+      Serial.println(" ");
+    */
+  }
+  prevPot = pot;
+
   currentSignal = digitalRead( MOTOR_SIGNAL_PIN );
   if ( previousSignal != currentSignal )
   {
-    previousSignal = currentSignal;
+
     if ( currentSignal )
     {
       // start moving
@@ -40,6 +69,7 @@ void loop() {
       // stop
       stepper.stop();
     }
+    previousSignal = currentSignal;
   }
   stepper.run();
 }
